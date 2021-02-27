e0feda6d8eff888b2127f01a9b710346c450a401
Ken Ho <kentsu.ho@gmail.com>
2021-02-27 11:20:08 +0800

    Deploy

---
+++ js/66.b1bc0ed1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[66],{"119":function(n,o,t){"use strict";t.r(o),t.d(o,"taro_block_core",(function(){return r}));var c=t(31),r=function(){function Block(n){Object(c.g)(this,n)}return Block.prototype.render=function(){return Object(c.f)(c.a,null)},Block}()}}]);
